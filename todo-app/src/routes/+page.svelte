2796a0dbc05494fa6519e5cef67383b93a0ce1d7
Antonin-Suzor <antonin@suzor.net>
2025-05-09 17:18:44 +0200

    Add basic TODO page structure

--- todo-app/src/routes/+page.svelte
+++ todo-app/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>TODO app</h1>
+<h3>Start small, then expand.</h3>
+<a href="/app">Go to the application</a>
---
+++ todo-app/src/routes/app/+page.svelte
@@ -0,0 +1,16 @@
+<h1>TODO app</h1>
+<a href="/">Go to the home page</a>
+
+<h3>TODO</h3>
+<div>
+    <p>TODO element</p>
+    <label><input type="checkbox">Done ?</label>
+    <p>Element description</p>
+</div>
+
+<h3>DONE</h3>
+<div>
+    <p>DONE element</p>
+    <label><input type="checkbox" checked>Done ?</label>
+    <p>Element description</p>
+</div>
