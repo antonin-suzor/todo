7ee432b87acf0280f1241a472904a8af6b9e6229
Antonin-Suzor <antonin@suzor.net>
2025-05-09 17:37:53 +0200

    Refactor TODO elements into a component

---
+++ todo-app/src/lib/TODOElement.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let {title, done = false, description}: { title: string, done?: boolean, description: string } = $props();
+</script>
+
+<div>
+    <p>{title}</p>
+    <label><input type="checkbox" checked={done}>Done ?</label>
+    <p>{description}</p>
+</div>
--- todo-app/src/routes/app/+page.svelte
+++ todo-app/src/routes/app/+page.svelte
@@ -1,16 +1,12 @@
+<script lang="ts">
+    import TODOElement from '$lib/TODOElement.svelte'
+</script>
+
 <h1>TODO app</h1>
 <a href="/">Go to the home page</a>
 
 <h3>TODO</h3>
-<div>
-    <p>TODO element</p>
-    <label><input type="checkbox">Done ?</label>
-    <p>Element description</p>
-</div>
+<TODOElement title={'TODO element'} description={'Element description'}/>
 
 <h3>DONE</h3>
-<div>
-    <p>DONE element</p>
-    <label><input type="checkbox" checked>Done ?</label>
-    <p>Element description</p>
-</div>
+<TODOElement title={'DONE element'} done={true} description={'Element description'}/>
