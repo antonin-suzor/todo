605ab7190f83529d8534058d1b34bbb782539ff3
Antonin-Suzor <antonin@suzor.net>
2025-05-09 22:20:05 +0200

    Add form to create new TODO elements

--- todo-app/src/routes/app/+page.svelte
+++ todo-app/src/routes/app/+page.svelte
@@ -17,6 +17,21 @@
         }
     ]);
 
+    let addTitle = $state('');
+    let addDone = $state(false);
+    let addDescription = $state('');
+    function addElement() {
+        elements.push({
+            id: Date.now(),
+            title: addTitle,
+            done: addDone,
+            description: addDescription,
+        });
+        addTitle = '';
+        addDone = false;
+        addDescription = '';
+    }
+
     function updateElementData(elementData: TODOElementData) {
         let i = elements.findIndex(e => e.id === elementData.id);
         elements[i] = elementData;
@@ -25,6 +40,14 @@
 
 <h1>TODO app</h1>
 <a href="/">Go to the home page</a>
+
+<h3>Add</h3>
+<form onsubmit={addElement}>
+    <label>Title: <input type="text" bind:value={addTitle}></label>
+    <label><input type="checkbox" bind:checked={addDone}>Done ?</label>
+    <label>Description: <input type="text" bind:value={addDescription}></label>
+    <input type="submit" value="Add">
+</form>
 
 <h3>TODO</h3>
 {#each elements.filter(e => !e.done) as elem (elem.id)}
