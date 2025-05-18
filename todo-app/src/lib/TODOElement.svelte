690a63b36377dd93b0ac4085465d82453507b06a
Antonin-Suzor <antonin@suzor.net>
2025-05-18 16:05:24 +0200

    Add intuitive way of changing TODO title

--- todo-app/src/lib/TODOElement.svelte
+++ todo-app/src/lib/TODOElement.svelte
@@ -14,10 +14,44 @@
             doneCheckbox.checked = data.done;
         }
     }
+
+    let changingTitle = $state(false);
+    let titleInput: HTMLInputElement;
+    function startChangingTitle() {
+        changingTitle = true;
+    }
+    /* Asks the parent to change the "title" value. If successful, will turn the UI back to normal. */
+    async function confirmChangeTitle() {
+        if (data.title === titleInput.value || await updateElementData({...data, title: titleInput.value})) {
+            changingTitle = false;
+        }
+    }
+    let resetTitleInput: HTMLButtonElement;
+    function resetChangeTitle() {
+        titleInput.value = data.title;
+        changingTitle = false;
+    }
+    /* Resets or confirms the changes to the title if the user presses Escape or Enter. */
+    function titleInputEscapeOrEnter(e: KeyboardEvent) {
+        if (e.key === 'Escape') {
+            resetChangeTitle();
+        }
+        else if (e.key === 'Enter') {
+            confirmChangeTitle();
+        }
+    }
 </script>
 
 <div style="border: solid 1px grey">
-    <p>{data.title}</p>
+    {#if !changingTitle}
+        <p onclick={startChangingTitle}>{data.title}</p>
+        <button onclick={startChangingTitle}>Change</button>
+    {:else}
+        <input type="text" value={data.title} bind:this={titleInput} autofocus
+               onfocusout={(e) => {if (e.relatedTarget !== resetTitleInput) confirmChangeTitle()}}
+               onkeydown={titleInputEscapeOrEnter}>
+        <button onclick={resetChangeTitle} bind:this={resetTitleInput}>Reset</button>
+    {/if}
     <label><input type="checkbox" checked={data.done} onchange={changeDone} bind:this={doneCheckbox}>Done ?</label>
     <p>{data.description}</p>
     <button onclick={() => deleteElement(data)}>Delete</button>
